9bf5768fbd0ec21595e32f8bfc19ce8ae8537a2e
mjeanelwood <85648554+mjeanelwood@users.noreply.github.com>
2021-09-21 16:23:50 -0700

    added github and netlify links

--- src/App.js
+++ src/App.js
@@ -7,7 +7,32 @@ export default function App() {
       <header className="App-header">
         <h2 className="title">Weather Search Engine</h2>
         <Weather />
+        <p>
+          <a
+            href="https://github.com/mjeanelwood/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>
+        </p>
       </header>
+      <footer>
+        {" "}
+        <p className="links">
+          <a
+            href="https://github.com/mjeanelwood/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source coding
+          </a>{" "}
+          by Melba Elwood | hosted by{" "}
+          <a href="" target="_blank" rel="noreferrer">
+            Netlify
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
