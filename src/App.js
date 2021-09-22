c5fa4be67f4f88fed92509302f4095dd3cc9676e
mjeanelwood <85648554+mjeanelwood@users.noreply.github.com>
2021-09-21 17:57:32 -0700

    removed extra app components

--- src/App.js
+++ src/App.js
@@ -4,27 +4,25 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2 className="title">Weather Search Engine</h2>
-        <Weather />
-        <p className="links">
-          <a
-            href="https://github.com/mjeanelwood/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open source code
-          </a>{" "}
-          by Melba Elwood | hosted by{" "}
-          <a
-            href="https://app.netlify.com/sites/focused-shockley-f75a75/deploys"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Netlify
-          </a>
-        </p>
-      </header>
+      <h2 className="title">Weather Search Engine</h2>
+      <Weather />
+      <p className="links">
+        <a
+          href="https://github.com/mjeanelwood/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open source code
+        </a>{" "}
+        by Melba Elwood | hosted by{" "}
+        <a
+          href="https://app.netlify.com/sites/focused-shockley-f75a75/deploys"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </p>
     </div>
   );
 }
