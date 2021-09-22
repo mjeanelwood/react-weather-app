b5dca4e547688327a0256c291131cc73de123313
mjeanelwood <85648554+mjeanelwood@users.noreply.github.com>
2021-09-21 17:07:18 -0700

    changed link text color

--- src/App.js
+++ src/App.js
@@ -7,32 +7,24 @@ export default function App() {
       <header className="App-header">
         <h2 className="title">Weather Search Engine</h2>
         <Weather />
-        <p>
-          <a
-            href="https://github.com/mjeanelwood/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open source code
-          </a>
-        </p>
-      </header>
-      <footer>
-        {" "}
         <p className="links">
           <a
             href="https://github.com/mjeanelwood/react-weather-app"
             target="_blank"
             rel="noreferrer"
           >
-            Open source coding
+            Open source code
           </a>{" "}
           by Melba Elwood | hosted by{" "}
-          <a href="https://www.netlify.com" target="_blank" rel="noreferrer">
+          <a
+            href="https://app.netlify.com/sites/focused-shockley-f75a75/deploys"
+            target="_blank"
+            rel="noreferrer"
+          >
             Netlify
           </a>
         </p>
-      </footer>
+      </header>
     </div>
   );
 }
